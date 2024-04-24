1ec3608540f8b9bd838a2ace54ed2e88ebbd7a30
ameliekou <ka7675@mci4me.at>
2024-04-24 14:17:39 +0200

    main.py added

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Eine Überschrift der ersten Ebene
+st.write("# EKG APP")
+
+# Eine Überschrift der zweiten Ebene
+st.write("## Versuchsperson auswählen")
+
+# Eine Auswahlbox
+current_user = st.selectbox(
+    'Versuchsperson',
+    options = ["Nutzer1", "Nutzer2"], key="sbVersuchsperson")
