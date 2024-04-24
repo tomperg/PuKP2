33a17f1aa42e4627a216517336521ed333f3dae2
tomperg <pt7063@mci4me.at>
2024-04-24 13:22:16 +0200

    Data

---
+++ Aufgabenstellung/5_build_dashboard/ekgdata.py
@@ -0,0 +1,28 @@
+import json
+import pandas as pd
+
+# %% Objekt-Welt
+
+# Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
+
+class EKGdata:
+
+## Konstruktor der Klasse soll die Daten einlesen
+
+    def __init__(self, ekg_dict):
+        pass
+        self.id = ekg_dict["id"]
+        self.date = ekg_dict["date"]
+        self.data = ekg_dict["result_link"]
+        self.df = pd.read_csv(self.data, sep='\t', header=None, names=['EKG in mV','Time in ms',])
+
+
+
+if __name__ == "__main__":
+    print("This is a module with some functions to read the EKG data")
+    file = open("data/person_db.json")
+    person_data = json.load(file)
+    ekg_dict = person_data[0]["ekg_tests"][0]
+    print(ekg_dict)
+    ekg = EKGdata(ekg_dict)
+    print(ekg.df.head())
