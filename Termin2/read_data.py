5af57372a0a1ba8f46c2598e69ac51957135a395
tomperg <pt7063@mci4me.at>
2024-04-24 16:03:32 +0200

    funktionen

--- Termin2/read_data.py
+++ Termin2/read_data.py
@@ -1,15 +1,28 @@
 import json
 
-'''# Opening JSON file
+# Opening JSON file
 file = open("data/person_db.json")
 
 # Loading the JSON File in a dictionary
 person_data = json.load(file)
 
-person_data'''
+print(person_data)
 
 def load_person_data():
     """A Function that knows where the person database is and returns a dictionary with the persons"""
+
     file = open("data/person_db.json")
+
     person_data = json.load(file)
+
     return person_data
+
+def get_person_list():
+    namenliste = []
+
+    for name in person_data: 
+        namenliste.append(name["firstname"] + ", " + name["lastname"])
+
+    return namenliste
+
+print(get_person_list())
