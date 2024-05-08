48044bf417c7bd86a1ac0919afc046766f27653d
tomperg <pt7063@mci4me.at>
2024-05-08 12:02:01 +0200

    first

---
+++ Termin_3/read_pandas.py
@@ -0,0 +1,36 @@
+
+
+# Paket für Bearbeitung von Tabellen
+import pandas as pd
+
+# Paket
+## zuvor !pip install plotly
+## ggf. auch !pip install nbformat
+import plotly.express as px
+path = "data/activities/activity.csv"
+
+def read_my_csv():
+    # Einlesen eines Dataframes
+    ## "\t" steht für das Trennzeichen in der txt-Datei (Tabulator anstelle von Beistrich)
+    ## header = None: es gibt keine Überschriften in der txt-Datei
+    df = pd.read_csv(path, sep="\t", header=None)
+
+    # Setzt die Columnnames im Dataframe
+    df.columns = ["Messwerte in mV", "Zeit in ms"]
+    
+    # Gibt den geladen Dataframe zurück
+    return df
+
+
+
+
+
+
+def make_plot(df):
+
+    # Erstellte einen Line Plot, der ersten 2000 Werte mit der Zeit aus der x-Achse
+    fig = px.line(df.head(2000), x= "Zeit in ms", y="Messwerte in mV")
+    return fig
+
+
+
