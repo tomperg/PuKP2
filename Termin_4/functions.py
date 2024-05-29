72c9f90423956eb87fd5fbe40b84c2c3c0385b8e
ameliekou <ka7675@mci4me.at>
2024-05-29 11:02:40 +0200

    all

--- Termin_4/functions.py
+++ Termin_4/functions.py
@@ -1,53 +1,47 @@
-# Paket für Bearbeitung von Tabellen
 import pandas as pd
-
-# Paket
-## zuvor !pip install plotly
-## ggf. auch !pip install nbformat
 import plotly.express as px
 import numpy as np
 
 
-def read_acivity_csv():
-
+def read_activity_csv(path="activities/activity.csv"):
     # Einlesen eines Dataframes
-    path = "activities/activity.csv"
-    df = pd.read_csv(path, sep=",")
-
-    # Gibt den geladen Dataframe zurück
+    ## "\t" steht für das Trennzeichen in der txt-Datei (Tabulator anstelle von Beistrich)
+    ## header = None: es gibt keine Überschriften in der txt-Datei
+    df = pd.read_csv(path)
+    #TODO add 'time' column DONE
+    df['time'] = [i for i in range(len(df))]
     return df
 
-def find_best_effort(df, t_interval, fs =1):
-    # Berechnung des besten Efforts
+def best_effort(df, t_interval, fs =1):
     windowsize = t_interval * fs
     meanpower = df["PowerOriginal"].rolling(window = windowsize).mean()
     bestpower = meanpower.max()
 
     return bestpower
 
-# Inervalle 
-def create_power_curce(df, fs = 1):
+ 
+def our_power_curve(df, fs = 1):
     intervall = np.array(range(len(df))) / fs
     powercurve = []
     
     for i in intervall:
         i = int(i)
-        powercurve.append(find_best_effort(df, i, fs))
+        powercurve.append(best_effort(df, i, fs))
     
 
     df_powercurve = pd.DataFrame({"Powercurve": powercurve, "Intervall": intervall/60})
     return df_powercurve
 
 if __name__ == "__main__":
-    df = read_acivity_csv()
+    df = read_activity_csv()
     #print(find_best_effort(df, 30))
     #print(df.head())
-    print(create_power_curce(df, 2))
+    print(our_power_curve(df, 2))
 
 
     #plotten
     time_intervals = [1, 5, 10, 30, 60, 300, 600, 1800, 3600]
-    fig = px.line(create_power_curce(df, 1), x= "Intervall", y="Powercurve", log_x=True, title='Logarithmische Skala auf der X-Achse')
+    fig = px.line(our_power_curve(df, 1), x= "Intervall", y="Powercurve", log_x=True, title='Logarithmische Skala auf der X-Achse')
     layout = fig.update_layout(title="Powercurve", xaxis_title="Intervall in Minuten", yaxis_title="Power in Watt")
    
 
