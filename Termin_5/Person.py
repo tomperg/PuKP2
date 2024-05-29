78e03635e4619c54a727f00842d4517e10d0382c
tomperg <pt7063@mci4me.at>
2024-05-29 12:03:16 +0200

    static methods

--- Termin_5/Person.py
+++ Termin_5/Person.py
@@ -1,11 +1,11 @@
 import json
-
+import datetime as dt
 class Person:
     
     @staticmethod
     def load_person_data():
         """A Function that knows where te person Database is and returns a Dictionary with the Persons"""
-        file = open("data/person_db.json")
+        file = open("person_db.json")
         person_data = json.load(file)
         return person_data
 
@@ -48,9 +48,39 @@ class Person:
         self.picture_path = person_dict["picture_path"]
         self.id = person_dict["id"]
 
+#Erstelle eine Funktion um das Alter der Personen aus dem JSON zu berechnen
+    def calc_age(self):
+        data_used_for_age_calc = Person.find_person_data_by_name(self)
+        birthdate = data_used_for_age_calc["date_of_birth"]
+        year = dt.date.today().year
+        """A Function to calculate the age of a person"""
+        age = year - birthdate
+        return age
+    
+
+    #Funktion um max_HR basierend auf Alter zu berechnen
+    def calc_max_heart_rate(self):
+        age_used = Person.calc_age(self)
+        max_heart_rate = 220 - age_used
+        return max_heart_rate
+        
+    def load_by_id(person_id):
+        person_data = Person.load_person_data()
+        for eintrag in person_data:
+            if eintrag["id"] == person_id:
+                return eintrag
+        else:
+            return {}
+        
+        
+        
+
 if __name__ == "__main__":
     print("This is a module with some functions to read the person data")
     persons = Person.load_person_data()
     person_names = Person.get_person_list(persons)
     print(person_names)
     print(Person.find_person_data_by_name("Huber, Julian"))
+    print(Person.calc_age("Huber, Julian"))
+    print(Person.calc_max_heart_rate("Heyer, Yannic"))
+    print(Person.load_by_id(3))
